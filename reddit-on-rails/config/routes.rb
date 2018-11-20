a254bd44a43e74db5ab6237a300dcef085dae21e
cstinepham <cstinepham@gmail.com>
2018-11-20 11:26:20 -0800

    Create post controller and migrate post_subs

--- reddit-on-rails/config/routes.rb
+++ reddit-on-rails/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
   
-  resources :subs, except: :destroy
+  resources :subs, except: :destroy do
+    resources :posts, only: [:create]
+  end
+  resources :posts, only: [:new, :edit, :update, :show, :destroy]
   
 end
